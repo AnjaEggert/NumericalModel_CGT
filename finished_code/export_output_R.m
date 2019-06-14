5e9dc4cd757af918f0f2dd7ecc7fec24e0e9cc6b
Anja <eggert@fbn-dummerstorf.de>
2019-06-14 14:02:49 +0200

    new export mat.file

--- finished_code/export_output_R.m
+++ finished_code/export_output_R.m
@@ -1,16 +1,18 @@
+% save all data starting with output_
 workspaceVars = who;
-
 findVars = strfind(workspaceVars, 'output_');
 indexVars = find(not(cellfun('isempty', findVars)));
 % Now you can use workspaceVars and indexVars along with save
 save('../results/output.mat',workspaceVars{indexVars})
 
+% save time_axis in a separate file
 % first convert matlab time to string
 datetime = datestr(time_axis);
-findVars1 = strfind(workspaceVars, 'datetime');
+workspaceVars1 = who;
+findVars1 = strfind(workspaceVars1, 'datetime');
 indexVars1 = find(not(cellfun('isempty', findVars1)));
 % Now you can use workspaceVars and indexVars along with save
-save('../results/datetime.mat',workspaceVars{indexVars1})
+save('../results/datetime.mat',workspaceVars1{indexVars1})
 
 
 
